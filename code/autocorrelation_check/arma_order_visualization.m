a3b824a09f9737404c80550c77fcbe76206196d2
Your Name <your.email@example.com>
2026-04-26 22:54:54 -0400

    arrange things

--- code/autocorrelation_check/arma_order_visualization.m
+++
@@ -1,2 +0,0 @@
-dat = fmri_data("\data\100307_ar_order_map.nii")
-orthviews(dat);
--- code/residual_contrast/PredictContrast_MOTOR.m
+++
@@ -1,5 +0,0 @@
-function PredictContrast_MOTOR(DATA_FILE, OUTDIR)
-COMP2 = nchoosek(1:6,2);          % all pairwise (15)
-cond_labels = {'cue','lf','rf','lh','rh','tongue'};
-run_observed_stage_flexible(DATA_FILE, OUTDIR, COMP2, cond_labels, 'MOTOR');
-end
